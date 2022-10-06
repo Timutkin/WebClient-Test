d7c27e08198a7bffc03476c052af7a64c03c51e9
Utkin <xXwild.duckXx@yandex.ru>
2022-10-06 15:32:29 +0300

    Init project

---
+++ src/main/java/ru/timutkin/testwebclient/TestWebClientApplication.java
@@ -0,0 +1,13 @@
+package ru.timutkin.testwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/timutkin/testwebclient/TestWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.timutkin.testwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
