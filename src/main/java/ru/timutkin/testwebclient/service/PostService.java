6c619ad24bbc052cd12787f2813cd8e4b83a8054
Utkin <xXwild.duckXx@yandex.ru>
2022-10-08 18:30:43 +0300

    Methods that receive post and user by is has been added

--- src/main/java/ru/timutkin/testwebclient/service/PostService.java
+++ src/main/java/ru/timutkin/testwebclient/service/PostService.java
@@ -1,8 +1,9 @@
 package ru.timutkin.testwebclient.service;
 
 import reactor.core.publisher.Mono;
+import ru.timutkin.testwebclient.dto.Post;
 import ru.timutkin.testwebclient.dto.User;
 
 public interface PostService {
-    Mono<User> getPostByIdAsync();
+    Mono<Post> getPostByIdAsync(Long id);
 }
